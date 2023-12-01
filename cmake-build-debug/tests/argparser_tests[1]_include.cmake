2577dca77e30f19e1432bdc85f9aa6dbd0bc84c4
zaimkin <Zaimkin.work@gmail.com>
2023-12-01 23:19:27 +0300

    OP4

---
+++ cmake-build-debug/tests/argparser_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/itmo/CLionProjects/labwork4-zxcbank — копия/cmake-build-debug/tests/argparser_tests[1]_tests.cmake")
+  include("C:/itmo/CLionProjects/labwork4-zxcbank — копия/cmake-build-debug/tests/argparser_tests[1]_tests.cmake")
+else()
+  add_test(argparser_tests_NOT_BUILT argparser_tests_NOT_BUILT)
+endif()
