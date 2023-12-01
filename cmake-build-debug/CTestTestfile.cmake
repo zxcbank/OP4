16d99119231ffc8bce27658c82bb9764507cf213
zxcbank <79111518+zxcbank@users.noreply.github.com>
2023-12-01 23:23:07 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/itmo/CLionProjects/labwork4-zxcbank — копия
-# Build directory: C:/itmo/CLionProjects/labwork4-zxcbank — копия/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("bin")
-subdirs("tests")
